b3e6b9fd8078e6eb4411e4333deec592c43c5984
Yahiaoui Rachid <rac021@hotmail.com>
2019-01-25 00:06:58 +0100

    Update CustomViewHandler.java

--- src/jaxy/src/main/java/com/rac021/jaxy/jsf/handler/CustomViewHandler.java
+++ src/jaxy/src/main/java/com/rac021/jaxy/jsf/handler/CustomViewHandler.java
@@ -6,7 +6,6 @@ package com.rac021.jaxy.jsf.handler ;
  *
  * @author ryahiaoui
  */
-import javax.servlet.ServletContext ;
 import javax.faces.component.UIViewRoot ;
 import javax.faces.context.FacesContext ;
 import javax.faces.application.ViewHandler ;
@@ -14,27 +13,22 @@ import javax.faces.application.ViewHandlerWrapper ;
 
 public class CustomViewHandler extends ViewHandlerWrapper {
 
-    private ViewHandler wrappped ;
-
-    public CustomViewHandler(ViewHandler wrappped)        {
-        super() ;
-    }
-
-    @Override
-    public ViewHandler getWrapped() {
-        return wrappped ;
+    public CustomViewHandler(ViewHandler wrapped)         {
+        super(wrapped) ;
     }
 
     @Override
     public UIViewRoot restoreView(FacesContext facesContext, String viewId)              {
 
+        /*
         ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance()
                                                                      .getExternalContext()
                                                                      .getContext()       ;
         
-        // String serverRealPath    = servletContext.getRealPath("/")                    ;
-        // String serverContextPath = servletContext.getContextPath()                    ;
-
+        String serverRealPath    = servletContext.getRealPath("/")                       ;
+        String serverContextPath = servletContext.getContextPath()                       ;
+        */
+        
         UIViewRoot root          = super.restoreView(facesContext, viewId)               ;
 
         if (root == null) {
