415fd8e21347ea6fd3dfe979fcb2519a3d28cee3
Yahiaoui Rachid <rac021@hotmail.com>
2019-02-02 02:51:50 +0100

    Update docker_builder.sh

--- jaxy/demo/16_test_monitoring/docker_builder.sh
+++ jaxy/demo/16_test_monitoring/docker_builder.sh
@@ -3,9 +3,6 @@
 
  docker build -t rac021/jaxy-grafana    -f grafana/Dockerfile grafana/
 
- docker build -t rac021/jaxy-prometheus -f prometheus/Dockerfile prometheus/
-
- docker push rac021/jaxy-grafana
-
- docker push rac021/jaxy-prometheus 
-
+ docker build -t rac021/jaxy-prometheus -f prometheus/Dockerfile prometheus
+ 
+ 
