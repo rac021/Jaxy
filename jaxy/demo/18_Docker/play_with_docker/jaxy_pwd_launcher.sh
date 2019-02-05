c9adbfa183e87b6226820e3ee83b84f5d8c0386c
Yahiaoui Rachid <rac021@hotmail.com>
2019-02-05 19:31:35 +0100

    Update jaxy_pwd_launcher.sh

--- jaxy/demo/18_Docker/play_with_docker/jaxy_pwd_launcher.sh
+++ jaxy/demo/18_Docker/play_with_docker/jaxy_pwd_launcher.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+DOCKER_LOGER="/app/jaxy/docker/docker.log"
+
 exportEnvirVariable()  {
 
    echo "export $1=$2" >> ~/.bashrc
@@ -10,12 +12,48 @@ exportEnvirVariable()  {
 if [ ! -f ~/.bashrc ]; then
     touch ~/.bashrc
 fi
+ 
+echo 
 
-DOCKER_LOGER="/app/jaxy/docker/docker.log"
+PWD_URL=""
 
-PWD_URL_LINE=`grep -m 1 'GET http://ip.*./v1' $DOCKER_LOGER | sed 's/\/v1.*//' | sed 's/^.*http:\/\///' | sed 's/*-*//' |  sort --unique `
+COUNT=10
 
-PWD_URL=`echo "${PWD_URL_LINE%-*}"`
+MAX_ATTEMP=20
+
+while [[ ! -n  "$PWD_URL"  ]] &&  [ $COUNT -lt $MAX_ATTEMP ] ; do           
+
+    while read -r url ; do 
+	
+       URL=`echo "${url%-*}"`
+	    URL="$URL-8181.direct.labs.play-with-docker.com"
+   	 echo " -- Check URL : $URL"   
+       
+       CODE_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $URL` ;
+
+       if [ "$CODE_RESPONSE" -ne "000" ] ; then 
+		        PWD_URL="$URL"
+		        break 
+       fi
+            
+    done < <(  grep -m 10 'GET http://ip.*./v1' ~/Téléchargements/PWD.txt | sed 's/\/v1.*//'       |  \
+                                                                            sed 's/^.*http:\/\///' |  \
+                                                                            sed 's/*-*//'          |  \
+                                                                            sort --unique             )
+    sleep 3
+    let "COUNT++"            
+    
+done
+
+if [[ ! -n "$$PWD_URL" ]] ; then 
+      echo
+      echo " NO PWD_URL EXTRACTED ! "
+      echo " Exit ... " ; echo 
+      exit     
+fi
+       
+echo ; echo " PWD_URL --> $PWD_URL" 
+echo ; echo 
 
 # Export Values 
 
@@ -25,7 +63,7 @@ exportEnvirVariable "KEYCLOAK_PORT" "8180"
 
 exportEnvirVariable "JAXY_URL"      "$PWD_URL-$JAXY_PORT.direct.labs.play-with-docker.com"
 
-exportEnvirVariable "KEYCLOAK_URL"  "http://$PWD_URL-$KEYCLOAK_PORT.direct.labs.play-with-docker.com"
+exportEnvirVariable "KEYCLOAK_URL"  "$PWD_URL-$KEYCLOAK_PORT.direct.labs.play-with-docker.com"
 
 echo 
 echo " ============================== "
