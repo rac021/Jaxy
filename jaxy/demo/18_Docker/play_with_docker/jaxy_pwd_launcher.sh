861bb1ffa3e600da9705110d72ba9b0636405ce0
Yahiaoui Rachid <rac021@hotmail.com>
2019-02-05 03:50:33 +0100

    Update jaxy_pwd_launcher.sh

--- jaxy/demo/18_Docker/play_with_docker/jaxy_pwd_launcher.sh
+++ jaxy/demo/18_Docker/play_with_docker/jaxy_pwd_launcher.sh
@@ -1,22 +1,31 @@
 #/bin/bash
 
+exportEnvirVariable()  {
+
+   echo "export $1=$2" >> ~/.bashrc
+   source ~/.bashrc
+
+}
+ 
+if [ ! -f ~/.bashrc ]; then
+    touch ~/.bashrc
+fi
+
 DOCKER_LOGER="/app/jaxy/docker/docker.log"
 
-PWD_URL_LINE=`grep -m 1 'GET http://ip.*./v1' $DOCKER_LOGER |  sed 's/\/v1.*//' | sed 's/^.*http:\/\///' | sed 's/*-*//'`
+PWD_URL_LINE=`grep -m 1 'GET http://ip.*./v1' $DOCKER_LOGER | sed 's/\/v1.*//' | sed 's/^.*http:\/\///' | sed 's/*-*//'`
 
 PWD_URL=`echo "${PWD_URL_LINE%-*}"`
 
-JAXY_PORT="8181"
+# Export Values 
 
-KEYCLOAK_PORT="8180"
+exportEnvirVariable "JAXY_PORT"     "8181"
 
-JAXY_URL="http://$PWD_URL-$JAXY_PORT.direct.labs.play-with-docker.com"
+exportEnvirVariable "KEYCLOAK_PORT" "8180"
 
-KEYCLOAK_URL="http://$PWD_URL-$KEYCLOAK_PORT.direct.labs.play-with-docker.com"
+exportEnvirVariable "JAXY_URL"      "http://$PWD_URL-$JAXY_PORT.direct.labs.play-with-docker.com"
 
-export JAXY_URL="$JAXY_URL"
-
-export KEYCLOAK_URL="$KEYCLOAK_URL"
+exportEnvirVariable "KEYCLOAK_URL"  "http://$PWD_URL-$KEYCLOAK_PORT.direct.labs.play-with-docker.com"
 
 echo 
 echo " ============================== "
