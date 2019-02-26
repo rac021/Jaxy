e7b1cb485ae70c96a001f481bbe762e0083b5507
Yahiaoui Rachid <rac021@hotmail.com>
2019-02-26 15:42:54 +0100

    Update ResponseWriterInterceptorContext.java

--- src/dependencies/01_jaxy-api/src/main/java/com/rac021/jaxy/api/root/ResponseWriterInterceptorContext.java
+++ src/dependencies/01_jaxy-api/src/main/java/com/rac021/jaxy/api/root/ResponseWriterInterceptorContext.java
@@ -8,24 +8,40 @@ package com.rac021.jaxy.api.root ;
 import java.time.Instant ;
 import java.time.Duration ;
 import java.io.IOException ;
+import java.util.logging.Level ;
 import javax.ws.rs.ext.Provider ;
+import java.util.logging.Logger ;
 import java.util.concurrent.TimeUnit ;
 import javax.ws.rs.ext.WriterInterceptor ;
-import javax.ws.rs.WebApplicationException ;
 import com.rac021.jaxy.api.metrics.Metrics ;
 import javax.ws.rs.ext.WriterInterceptorContext ;
+import static com.rac021.jaxy.api.logger.LoggerFactory.getLogger ;
 import static com.rac021.jaxy.api.root.ConcurrentUsersManager.tryingReleaseSemaphore ;
 
 @Provider
 public class ResponseWriterInterceptorContext implements WriterInterceptor {
     
+    protected static final  Logger LOGGER   = getLogger()  ;
+    
     @Override
-    public void aroundWriteTo( WriterInterceptorContext context ) throws WebApplicationException, IOException {
+    public void aroundWriteTo( WriterInterceptorContext context ) throws IOException {
 
         try {
-            context.proceed();
+            
+            context.proceed()   ;
 
-        } finally {
+        } catch( Exception ex ) {
+        
+            LOGGER.log(Level.SEVERE, ex.getMessage(), ex )     ;
+            Metrics.incTotalExceptions()                       ;
+            
+            context.getOutputStream()
+                   .write( ( "\n<status> RuntimeException - "  +
+                              ex.getLocalizedMessage()         +
+                              " </status>\n\n" ) .getBytes() ) ;            
+        }
+        
+        finally {
 
             if ( RuntimeServiceInfos.STARTED_TIME.get() != null ) {
 
@@ -38,19 +54,19 @@ public class ResponseWriterInterceptorContext implements WriterInterceptor {
                 
                 Metrics.updateTimerService( timer, timeElapsed, TimeUnit.MILLISECONDS) ;
 
-                RuntimeServiceInfos.STARTED_TIME.remove();
-                RuntimeServiceInfos.SERVICE_NAME.remove();
-
+                RuntimeServiceInfos.STARTED_TIME.remove() ;
+                RuntimeServiceInfos.SERVICE_NAME.remove() ;
             }
 
             /**
              * RELEASE SEMAPHORE . *
              */
             if (RuntimeServiceInfos.SEMAPHORE_CURRENT_THREAD_NAME.get() != null) {
-                tryingReleaseSemaphore() ;
-                RuntimeServiceInfos.SEMAPHORE_CURRENT_THREAD_NAME.remove() ;
+                tryingReleaseSemaphore()                                         ;
+                RuntimeServiceInfos.SEMAPHORE_CURRENT_THREAD_NAME.remove()       ;
 
             }
         }
     }
 }
+
