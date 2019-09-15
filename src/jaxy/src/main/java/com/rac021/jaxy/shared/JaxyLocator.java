8a81981e587bae577dbecb4ae54ac72f5e278e96
Yahiaoui Rachid <rac021@hotmail.com>
2019-09-15 13:13:53 +0200

    Update JaxyLocator.java

--- src/jaxy/src/main/java/com/rac021/jaxy/shared/JaxyLocator.java
+++ src/jaxy/src/main/java/com/rac021/jaxy/shared/JaxyLocator.java
@@ -13,10 +13,7 @@ public class JaxyLocator {
     
     public static String getJaxyJarLocation() {
         
-        String jarFileLocation = System.getProperty("java.class.path")
-                                       .split(":" + System.getProperty("java.io.tmpdir"))[0] ;
-        
-        return new File(jarFileLocation).getParentFile().getAbsolutePath()                   ;
+       return System.getProperty("user.dir")  ;  
     }
        
 }
