e91f9b4ac6702a5bade0b14ee9779bc0ab36be5f
Yahiaoui Rachid <r.yahiaoui.21@gmail.com>
2020-03-06 21:08:44 +0100

    Update RequestInterceptor.java

--- src/dependencies/01_jaxy-api/src/main/java/com/rac021/jaxy/api/root/RequestInterceptor.java
+++ src/dependencies/01_jaxy-api/src/main/java/com/rac021/jaxy/api/root/RequestInterceptor.java
@@ -2,6 +2,7 @@
  package com.rac021.jaxy.api.root ;
 
  import javax.ws.rs.ext.Provider ;
+ import javax.ws.rs.core.Response ;
  import javax.ws.rs.container.PreMatching ;
  import javax.ws.rs.container.ContainerRequestFilter ;
  import javax.ws.rs.container.ContainerRequestContext ;
@@ -18,10 +19,20 @@
     @Override
     public void filter(ContainerRequestContext requestContext) {
         
-         String accept = requestContext.getHeaders().getFirst("Accept")     ;
-          if (accept == null || accept.equals("*/*")) {
-              requestContext.getHeaders().putSingle("Accept", "json/plain") ;
-          }
+        if ( requestContext.getMethod() != null  && 
+             requestContext.getMethod().equals("OPTIONS")) {
+            // Handling preflight requests - CORS 
+            requestContext.abortWith( Response.status(Response.Status.OK )
+                          .header("Access-Control-Allow-Credentials", "true")
+                          .header("Access-Control-Allow-Headers", "*")
+                          .header("Access-Control-Allow-Methods", "OPTIONS, HEAD, GET")
+                          .build() ) ;
+        }
+        
+        String accept = requestContext.getHeaders().getFirst("Accept")     ;
+        if (accept == null || accept.equals("*/*")) {
+             requestContext.getHeaders().putSingle("Accept", "json/plain") ;
+        }
     }
 
  }
