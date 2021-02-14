7c459b1031c6db6099378e994abaf74714d3d5a9
Yahiaoui Rachid <r.yahiaoui.21@gmail.com>
2021-02-15 00:16:05 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -54,22 +54,24 @@
   cd $CURRENT_LOCATION/dependencies/05_jaxyClient &&  mvn clean install assembly:single -Dmaven.test.skip=true $MVN_HTTPS_PROTOCOLS
   
   if [[ "$?" -ne 0 ]] ; then
+  
      echo ; echo 'Could not perform mvn clean install assembly:single -Dmaven.test.skip=true' ; echo 
      exit 21
+
   else 
+  
+     if [ ! -d "../../jaxy/src/main/resources/jaxy-client/" ]; then
+  
+        mkdir ../../jaxy/src/main/resources/jaxy-client/ # Create the directory jaxy-client in the jaxy project
+     fi
+  
+     mv target/jaxyClient.jar ../../jaxy/src/main/resources/jaxy-client/jaxyClient.jar
+   
+     rm -rf $CURRENT_LOCATION/dependencies/05_jaxyClient/target
+ 
      mvn clean 
   fi
-
-  if [ ! -d "../../jaxy/src/main/resources/jaxy-client/" ]; then
-  
-    mkdir ../../jaxy/src/main/resources/jaxy-client/ # Create the directory jaxy-client in the jaxy project
-  
-  fi
-  
-  mv target/jaxyClient.jar ../../jaxy/src/main/resources/jaxy-client/jaxyClient.jar
-  
-  rm -rf $CURRENT_LOCATION/dependencies/05_jaxyClient/target
-  
+ 
   cd $CURRENT_LOCATION/jaxy/
   
   if [ "$1" == "m2repo" ]; then
@@ -87,32 +89,35 @@
   fi
   
   if [[ "$?" -ne 0 ]] ; then
+  
      echo ; echo 'Could not perform mvn clean install -Dmaven.test.skip=true' ; echo 
      exit 21
+     
   else 
-     mvn clean 
+
+     # Copy jaxy-thorntail to $DEMO_PATH
+
+     if [ -f "$CURRENT_LOCATION/jaxy/target/jaxy-thorntail.jar" ]; then
+
+       if [ -f "$DEMO_PATH/jaxy-thorntail.jar" ]; then
+
+           rm $DEMO_PATH/jaxy-thorntail.jar    
+       fi
+
+       mv $CURRENT_LOCATION/jaxy/target/jaxy-thorntail.jar ../../$DEMO_PATH/jaxy-thorntail.jar
+
+       rm -rf $CURRENT_LOCATION/jaxy/target
+       
+       mvn clean 
+
+     else
+
+       echo ; echo " Error, Jaxy Jar : $CURRENT_LOCATION/jaxy/target/jaxy-thorntail.jar not found !! " ; echo 
+       echo "Target : " ; ls $CURRENT_LOCATION/jaxy/target/
+       exit 21
+
+     fi
+
   fi 
   
-  # Copy jaxy-thorntail to $DEMO_PATH
   
-  if [ -f "$CURRENT_LOCATION/jaxy/target/jaxy-thorntail.jar" ]; then
- 
-     if [ -f "$DEMO_PATH/jaxy-thorntail.jar" ]; then
-    
-         rm $DEMO_PATH/jaxy-thorntail.jar    
-     fi
-    
-     mv $CURRENT_LOCATION/jaxy/target/jaxy-thorntail.jar ../../$DEMO_PATH/jaxy-thorntail.jar
- 
-     rm -rf $CURRENT_LOCATION/jaxy/target
- 
-  else 
-  
-    echo 
-    echo " Error, Jaxy Jar : $CURRENT_LOCATION/jaxy/target/jaxy-thorntail.jar not found !! "
-    echo 
-    echo "Target : "
-    ls $CURRENT_LOCATION/jaxy/target/
-    exit 21
-    
-  fi
